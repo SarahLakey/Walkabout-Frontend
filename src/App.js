ac16f73ed0a019631dbe8172710d0ffad3cbaa1b
SarahLakey <salbers1212@gmail.com>
2024-07-03 17:21:26 -0500

    working on registering users

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from './pages/dashboard/Dashboard';
 import NoMatch from './pages/noMatch/NoMatch';
+import RegisterUser from './pages/register/RegisterUser';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Header/>
       <Routes>
         <Route path='/' element={<Dashboard/>} />
+        <Route path='/register' element={<RegisterUser/>} />
         <Route path='*' element={<NoMatch/>} />
 
       </Routes>
--- src/pages/register/RegisterUser.js
+++ src/pages/register/RegisterUser.js
@@ -1,10 +1,88 @@
+import { useState } from "react";
 import "./RegisterUser.css"
+import { Button, Form } from "react-bootstrap";
 
 const RegisterUser = () => {
 
+    const [formData, setForm] = useState({
+        firstName: "",
+        lastName: "",
+        email: "",
+        username: "",
+        password: ""
+
+    })
+
+    const handleInputChange = (event) => {
+        const {name, value} = event.target;
+        setFormData =({
+            ...formData,
+            [name]:value,
+        })
+    }
+
     return(
         <>
-        
+            <div className="center-form">
+                <h1>Register New User</h1>
+                <Form>
+                    <Form.Group controlId="formBasicName">
+                        <Form.Control
+                            type="text"
+                            name="firstName"
+                            placeholder="Enter first name"
+                            value= {formData.firstName}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicName">
+                        <Form.Control
+                            type="text"
+                            name="lastName"
+                            placeholder="Enter last name"
+                            value= {formData.lastName}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicName">
+                        <Form.Control
+                            type="email"
+                            name="email"
+                            placeholder="Enter email"
+                            value= {formData.email}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicName">
+                        <Form.Control
+                            type="text"
+                            name="username"
+                            placeholder="Enter username"
+                            value= {formData.username}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Form.Group controlId="formBasicName">
+                        <Form.Control
+                            type="text"
+                            name="password"
+                            placeholder="Enter password"
+                            value= {formData.password}
+                            onChange={handleInputChange}
+                        />
+                    </Form.Group>
+
+                    <Button variant="primary" type="submit" className="w-100">
+                        Register User
+                    </Button>
+
+
+                </Form>
+            </div>
         </>
 
     )
